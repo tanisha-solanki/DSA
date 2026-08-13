9c1c1506153cb34ed2d82ade141d47b46b328121
Tanisha Solanki <tanisha251205@gmail.com>
2026-08-13 22:48:44 +0530

    Time: 0 ms (100.00%) | Memory: 43.9 MB (47.58%) - LeetSync

---
+++ 88-merge-sorted-array/merge-sorted-array.java
@@ -0,0 +1,24 @@
+class Solution {
+    public void merge(int[] arr1, int m, int[] arr2, int n) {
+          int i=m-1,j=n-1,k=m+n-1;
+          while(i>=0 && j>=0){
+            if(arr1[i]>arr2[j]){
+                arr1[k]=arr1[i];
+                k--;
+                i--;
+            }
+            else {
+                arr1[k] = arr2[j];
+                k--;
+                j--;
+            }
+
+          } 
+          while(j>=0){
+            arr1[k] = arr2[j];
+            k--;
+            j--;
+          }
+        }
+    
+}
